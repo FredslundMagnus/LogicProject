3b24379072e22371091a5aac7b98e6a84f655eaa
snow1 <15653507629@163.com>
2021-11-21 18:06:41 +0100

    fix

--- graph.py
+++ graph.py
@@ -8,8 +8,8 @@ class Graph:
         self.states: list[State] = [State(p) for p in permutations(range(9),6) if State.isValid(p)]
         self.edges: dict[tuple[State, State], set[int]] = {(a, b): Graph.start_edges(a,b) for a, b in product(self.states, self.states)}
         self.state: State = choice(self.states)
-        print(len(self.states))
-        print(len(self.edges))
+        print(len(self.states), len(self.states)//6)
+        print(len(self.edges), len(self.edges)//36)
         print(len([1 for e in self.edges.values() if e]))
 
     @staticmethod
