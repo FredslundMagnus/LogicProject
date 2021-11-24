9d7a95f163da2af6dd44a75c6de8ca41f6ffd2ae
FredslundMagnus <s183905@student.dtu.dk>
2021-11-24 10:18:00 +0100

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from game import Game, Players
 game = Game(Players.Computer, Players.Computer, Players.Computer)
 
 game.current_beliefs()
-for round in range(1, 6):
+for round in range(1, 4):
     print(f"Round {round}!")
     for i in range(3):
         game.update(i, Game.sample())
