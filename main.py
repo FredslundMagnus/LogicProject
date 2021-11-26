56e45eee8b0410db3370380f70805cb57759eb63
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-11-26 14:50:33 +0100

    fix

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
+from __future__ import annotations
 from game import Game, Players
+from contextlib import redirect_stdout as printer
 
-game = Game(Players.Computer, Players.Computer, Players.Computer, do_print=False, rounds=2, depth=0)
+
+all_scores: list[list[int]] = []
+did_agree: list[list[bool]] = []
+
+n: int = 100
 
 
-for round in game:
-    for i in range(3):
-        game.update(i, Game.sample())
-    game.current_beliefs()
-    game.share()
+with printer(None):
+    for depth in range(5):
+        all_scores.append([])
+        did_agree.append([])
+        for _ in range(n):
+            game = Game(Players.Computer, Players.Computer, Players.Computer, do_print=False, rounds=2, depth=depth)
 
-print(game.should_guesses)
-print([g[0] == g[1] for g in game.should_guesses])
-print(sum([int(g[0] == g[1]) for g in game.should_guesses]))
-print(game.scores)
+            for round in game:
+                for i in range(3):
+                    game.update(i, Game.sample())
+                game.current_beliefs()
+                game.share()
+
+            print(game.should_guesses)
+            print([g[0] == g[1] for g in game.should_guesses])
+            print(sum([int(g[0] == g[1]) for g in game.should_guesses]))
+            print(game.scores)
+            print(sum(game.scores))
+            all_scores[depth].extend(game.scores)
+            did_agree[depth].extend(g[0] == g[1] for g in game.should_guesses)
+
+print(all_scores)
+print(did_agree)
+print("\n\n")
+print([sum(a) for a in all_scores])
+print([f"{sum(a)/len(a):.2f}" for a in all_scores])
+print([sum(a) for a in did_agree])
+print([f"{sum(a)/len(a)*100:.2f}%" for a in did_agree])
