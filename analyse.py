96eb70091158324f558075982efe356a45879258
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-11-29 11:19:17 +0100

    Fix

--- analyse.py
+++ analyse.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join
 
+all_scores = [[], [], [], [], []]
+did_agree = [[], [], [], [], []]
+for ending in (f"_{i+1}" for i in range(3)):
+    with open(join("data", f"all_scores{ending}.pickle"), 'rb') as f:
+        all_scores = [a+b for a, b in zip(all_scores, load(f))]
 
-with open(join("data", "all_scores_3.pickle"), 'rb') as f:
-    all_scores = load(f)
-
-with open(join("data", "did_agree_3.pickle"), 'rb') as f:
-    did_agree = load(f)
+    with open(join("data", f"did_agree{ending}.pickle"), 'rb') as f:
+        did_agree = [a+b for a, b in zip(did_agree, load(f))]
 
 print([sum(a) for a in all_scores])
 mean_scores = [sum(a)/len(a) for a in all_scores]
@@ -20,7 +22,11 @@ print([f"{p*100:.2f}%" for p in did_agree_percentage])
 print([(1.96 * np.std(a)/np.sqrt(len(a)))/len(a) for a in did_agree])
 
 plt.plot(mean_scores)
-plt.show()
+plt.title("Mean Score")
+plt.savefig(join("plots", "Mean_Score.png"))
+plt.clf()
 
 plt.plot(did_agree_percentage)
-plt.show()
+plt.title("Agreeness")
+plt.savefig(join("plots", "Agreeness.png"))
+plt.clf()
