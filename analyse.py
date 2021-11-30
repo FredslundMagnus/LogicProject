a1d22f3767758ceb10348f8fe4c5c947c0ee99bf
TheDingodile <jakob@3esser.dk>
2021-11-30 23:52:52 +0100

    fixed_bug

--- analyse.py
+++ analyse.py
@@ -17,15 +17,15 @@ if __name__ == "__main__":
 
         with open(join("data", f"did_agree{ending}.pickle"), 'rb') as f:
             did_agree = [a+b for a, b in zip(did_agree, load(f))]
-
+            
     print([sum(a) for a in all_scores])
     mean_scores = [sum(a)/len(a) for a in all_scores]
     print([f"{s:.2f}" for s in mean_scores])
-    print([(1.96 * np.std(a)/np.sqrt(len(a)))/len(a) for a in all_scores])
+    print([(1.96 * np.std(a)/np.sqrt(len(a))) for a in all_scores])
     print([sum(a) for a in did_agree])
     did_agree_percentage = [sum(a)/len(a) for a in did_agree]
     print([f"{p*100:.2f}%" for p in did_agree_percentage])
-    print([(1.96 * np.std(a)/np.sqrt(len(a)))/len(a) for a in did_agree])
+    print([(1.96 * np.std(a)/np.sqrt(len(a))) for a in did_agree])
 
     def plot(data: list[float], title: str):
         plt.xticks(range(5))
